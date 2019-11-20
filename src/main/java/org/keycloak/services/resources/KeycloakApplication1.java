d5566deef3c6918bbb74bf319e79fc57704e1d5a
Sergeev Vladimir <vld.152.09@gmail.com>
2019-11-20 14:38:40 +0300

    Update

--- src/main/java/org/keycloak/services/resources/KeycloakApplication1.java
+++ src/main/java/org/keycloak/services/resources/KeycloakApplication1.java
@@ -1,32 +1,104 @@
 package org.keycloak.services.resources;
 
+import org.apache.commons.io.FileUtils;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.core.Dispatcher;
+import org.keycloak.exportimport.ExportImportManager;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionTask;
+import org.keycloak.models.dblock.DBLockManager;
+import org.keycloak.models.dblock.DBLockProvider;
+import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.services.managers.ApplianceBootstrap;
 import org.keycloak.services.resources.KeycloakApplication;
 import org.keycloak.services.resources.WelcomeResource;
 import org.keycloak.services.resources.WelcomeResource1;
 
+//import java.util.concurrent.atomic;
 import javax.servlet.ServletContext;
 import javax.ws.rs.ConstrainedTo;
 import javax.ws.rs.core.Context;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class KeycloakApplication1 extends KeycloakApplication {
 
     private static final Logger logger = Logger.getLogger(KeycloakApplication.class);
+    protected Set<Object> mySingletons = new HashSet<Object>();
+
+    private boolean marker;
+    private boolean registrationMarker=false;
+    private boolean emptyFile=false;
 
     public KeycloakApplication1(@Context ServletContext context,@Context Dispatcher dispatcher) {
-
         super(context ,dispatcher);
-        for (Object o : singletons) {
-            if (o instanceof WelcomeResource) {
-                singletons.remove(o);
-                break;
+            final AtomicBoolean bootstrapAdminUser = new AtomicBoolean(false);
+            KeycloakModelUtils.runJobInTransaction(sessionFactory, new KeycloakSessionTask() {
+                @Override
+                public void run(KeycloakSession session) {
+                    boolean shouldBootstrapAdmin = new ApplianceBootstrap(session).isNoMasterUser();
+                    bootstrapAdminUser.set(shouldBootstrapAdmin);
+                }
 
+            });
+
+        for(Object o:singletons) {
+            if(!(o instanceof WelcomeResource))
+            {
+                mySingletons.add(o);
+            }
+            else{
+                firstUserRegistrationReader();
+                checkFile();
+                marker=bootstrapAdminUser.get();
+                if ((marker==true) && (registrationMarker==true)) {
+                    mySingletons.add(new WelcomeResource1(false));
+                    singletons=mySingletons;}
+                if((marker==true)&&(registrationMarker==false)&&(emptyFile==true)) {
+                    mySingletons.add(new WelcomeResource1(true));
+                    singletons=mySingletons;
+                }
             }
         }
-        AtomicBoolean bootstrapAdminUser = new AtomicBoolean(false);
-        singletons.add(new WelcomeResource1(bootstrapAdminUser.get()));
-
     }
+
+    private  void checkFile()
+    {
+        try(FileInputStream inFile = new FileInputStream("D://NetCracker//Keycloak//keycloak-7.0.0//keycloak-7.0.0//FirstAdminValidation.txt")) {
+
+            int b = inFile.read();
+            if (b == -1)
+            {
+                emptyFile=true;
+            }
+
+        }
+        catch(IOException ex)
+        {
+            logger.warn("File not found", ex);
+        }
+    }
+
+    private  void firstUserRegistrationReader()
+    {
+        try(FileInputStream inFile = new FileInputStream("D://NetCracker//Keycloak//keycloak-7.0.0//keycloak-7.0.0//FirstAdminValidation.txt"))
+        {
+            int i=-1;
+            while((i=inFile.read())!=-1){
+
+                if(i=='1')
+                {
+                    registrationMarker=true;
+                }
+            }
+        }
+
+        catch(IOException ex)
+        {
+            logger.warn("File not found", ex);
+        }
+    }
+
 }
--- src/main/java/org/keycloak/services/resources/WelcomeResource1.java
+++ src/main/java/org/keycloak/services/resources/WelcomeResource1.java
@@ -1,8 +1,82 @@
 package org.keycloak.services.resources;
 
+import org.keycloak.common.ClientConnection;
+import org.keycloak.common.Version;
+import org.keycloak.common.util.Base64Url;
+import org.keycloak.models.BrowserSecurityHeaders;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.services.ForbiddenException;
+import org.keycloak.services.ServicesLogger;
+import org.keycloak.services.Urls;
+import org.keycloak.services.managers.ApplianceBootstrap;
+import org.keycloak.services.util.CacheControlUtil;
+import org.keycloak.services.util.CookieHelper;
+import org.keycloak.theme.BrowserSecurityHeaderSetup;
+import org.keycloak.theme.FreeMarkerUtil;
+import org.keycloak.theme.Theme;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class WelcomeResource1 extends WelcomeResource {
 
+
+    private static final String KEYCLOAK_STATE_CHECKER = "WELCOME_STATE_CHECKER";
+    @Context
+    private KeycloakSession session;
+
+    @Context
+    protected HttpHeaders headers;
+
+    private boolean bootstrap;
+
     public WelcomeResource1(boolean bootstrap) {
+
         super(bootstrap);
+        this.bootstrap=bootstrap;
+
     }
+
+    @Override
+    public Response createUser(MultivaluedMap<String, String> formData) {
+        checkBootstrap(bootstrap);
+        return super.createUser(formData);
+    }
+
+    private  void checkBootstrap(boolean bootstrap)
+    {
+        if(bootstrap==true)
+        {
+            firstUserRegistrationWriter();
+        }
+    }
+    private void firstUserRegistrationWriter()
+    {
+        try (FileOutputStream outFile = new FileOutputStream("D://NetCracker//Keycloak//keycloak-7.0.0//keycloak-7.0.0//FirstAdminValidation.txt"))
+        {
+
+            String text = "1";
+            byte[] buffer = text.getBytes();
+            outFile.write(buffer, 0, buffer.length);
+        }
+
+        catch(IOException ex)
+        {
+            logger.warn("File not found", ex);
+        }
+    }
+
+
 }
